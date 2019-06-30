debff7051340fad5f63fc891bfa6b10e44a01277
Guram <guramidgebuadze@gmail.com>
2019-06-30 22:28:49 +0400

    new font, adding js for color change, changing sizes for resp

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+let guro =[];
+let mari = document.querySelectorAll('a');
 
 document.querySelector('.m-header-btn').addEventListener('mouseover', () => {
+
+	// --------- show header input for searching
 	document.querySelector('.m-header-form').classList.add('search-form');
 	document.querySelector('.m-header-input').classList.add('search-input');
 	document.querySelector('.m-header-btn').classList.add('search-btn');
+
+	// --------- change a color in header list
+	for(let i =0; i<= 7; i++) {
+	   guro.push(mari[i]);
+	   guro[i].style.color = '#3d719f';
+   }
 });
 
+
 document.querySelector('.m-header-input').addEventListener('mouseout', () => {
+
+	// --------- hide header input for searching 
 	document.querySelector('.m-header-form').classList.remove('search-form');
 	document.querySelector('.m-header-input').classList.remove('search-input');
 	document.querySelector('.m-header-btn').classList.remove('search-btn');
+
+    // --------- bring back a color in header list
+	for(let i =0; i<= 7; i++) {
+	   guro.push(mari[i]);
+	   guro[i].style.color = '#fff';
+   }
 });
 
